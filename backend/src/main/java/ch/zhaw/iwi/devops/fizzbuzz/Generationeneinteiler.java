2543aa369e50d104239bf1b790e0ab25b6e779f6
berchjan <91776874+berchjan@users.noreply.github.com>
2022-03-29 22:36:30 +0200

    Add Unit Testing

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
@@ -2,8 +2,23 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 
 public class Generationeneinteiler {
     public String generationeneinteiler(int zahl){
-        if (zahl >=2010){
+        if (zahl >=2011){
             return "Generation Alpha";
+        }
+        else if (zahl >=1997 && zahl <=2010){
+            return "Generation Z";
+        }
+        else if (zahl >=1981 && zahl <=1996){
+            return "Generation Y";
+        }
+        else if (zahl >=1965 && zahl <=1980){
+            return "Generation X";
+        }
+        else if (zahl >=1946 && zahl <=1964){
+            return "Boomer";
+        }
+        else if (zahl >=1928 && zahl <=1945){
+            return "Silent";
         }
         return "keine bestimmte Generation";
     }
