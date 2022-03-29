df0b94bc36ef5133e12c65ba7e75affb6621546f
berchjan <91776874+berchjan@users.noreply.github.com>
2022-03-29 22:00:45 +0200

    Add 1. Test and 1. Code

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
@@ -0,0 +1,10 @@
+package ch.zhaw.iwi.devops.fizzbuzz;
+
+public class Generationeneinteiler {
+    public String generationeneinteiler(int zahl){
+        if (zahl >=2010){
+            return "Generation Alpha";
+        }
+        return "keine bestimmte Generation";
+    }
+}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationeneinteilerTest.java
@@ -0,0 +1,12 @@
+package ch.zhaw.iwi.devops.fizzbuzz;
+import org.junit.Assert;
+import org.junit.Test;
+public class GenerationeneinteilerTest {
+    @Test
+
+	public void generationeinteilerAlpha(){
+		Generationeneinteiler ac = new Generationeneinteiler();
+		Assert.assertEquals("Generation Alpha", ac.generationeneinteiler(2014));
+}
+}
+    
