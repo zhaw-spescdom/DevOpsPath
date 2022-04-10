916cfdf0dd3c800c5498972cbd1de640ffdbbe90
berchjan <91776874+berchjan@users.noreply.github.com>
2022-04-10 10:09:08 +0200

    1. tested code

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
@@ -2,5 +2,10 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 
 public class Generationeneinteiler {
     public String generationeneinteiler(int zahl){
+        
+            if (zahl >=2011){
+                return "Generation Alpha";
+            }
+            return "keine bestimmte Generation";
     }
 }
