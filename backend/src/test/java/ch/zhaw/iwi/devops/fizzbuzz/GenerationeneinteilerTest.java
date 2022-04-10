41fc3dd07e8d0f118b50d276779b2fb0c0a6b2a2
berchjan <91776874+berchjan@users.noreply.github.com>
2022-04-10 09:47:07 +0200

    1. test

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationeneinteilerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationeneinteilerTest.java
@@ -8,49 +8,5 @@ public class GenerationeneinteilerTest {
 		Generationeneinteiler ac = new Generationeneinteiler();
 		Assert.assertEquals("Generation Alpha", ac.generationeneinteiler(2014));
 }
-@Test
-public void generationeinteilerGenZ(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation Z", ac.generationeneinteiler(2005));
-}
-@Test
-public void generationeinteilerGenY(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation Y", ac.generationeneinteiler(1993));
-}
-@Test
-public void generationeinteilerGenX(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation X", ac.generationeneinteiler(1975));
-}
-@Test
-public void generationeinteilerBoomer(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Boomer", ac.generationeneinteiler(1950));
-}
-@Test
-public void generationeinteilerSilent(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Silent", ac.generationeneinteiler(1933));
-}
-@Test
-public void generationeinteilerX(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation X", ac.generationeneinteiler(1966));
-}
-@Test
-public void generationeinteilerY(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation Y", ac.generationeneinteiler(1988));
-}
-@Test
-public void generationeinteilernon(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("keine bestimmte Generation", ac.generationeneinteiler(1922));
-}
-@Test
-public void generationeinteilerZ(){
-    Generationeneinteiler ac = new Generationeneinteiler();
-    Assert.assertEquals("Generation Z", ac.generationeneinteiler(2000));
-}
+
 }    
