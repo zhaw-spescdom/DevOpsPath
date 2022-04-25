d6f7a7020e52ac6f5aa4382c3471b0d3690987e2
berchjan <91776874+berchjan@users.noreply.github.com>
2022-04-25 20:25:44 +0200

    Generationeinteiler Bugfixing

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/Generationeneinteiler.java
@@ -2,7 +2,6 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 
 public class Generationeneinteiler {
     public String generationeneinteiler(int zahl){
-        
             if (zahl >=2011){
                 return "Generation Alpha";
             }
