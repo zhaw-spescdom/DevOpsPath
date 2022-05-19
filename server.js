538809d6394cd8e53be2ec6e568a90c037670798
Adrian Brügger <63962845+adrianbruegger@users.noreply.github.com>
2022-05-19 18:49:53 +0200

    changes

--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 
 
 const express = require('express');
+const path = require('path');
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', function(req, res) {
-    res.sendFile('./public/index.html');
+    res.sendFile(path.join(__dirname, '/index.html'));
   });
-  
 app.listen(PORT, HOST);
 console.log(`Running on
 http://${HOST}:${PORT}`);
