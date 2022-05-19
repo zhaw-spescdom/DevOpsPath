0f0714a33659e3923bf91cc0a0079a4dbfb2eeb5
Adrian Brügger <63962845+adrianbruegger@users.noreply.github.com>
2022-05-19 18:42:43 +0200

    added sendfile

--- server.js
+++ server.js
@@ -7,44 +7,10 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 // App
 const app = express();
-app.get('/', (req, res) => {
-res.send(
-    <html lang="en" >
-    <head>
-      <meta charset="UTF-8" />
-      <title>CodePen - Full Page Scrolling</title>
-      <link rel="stylesheet" href="./style.css" />
-    
-    </head>
-    <body>
-    <div class="fixed">
-        <div class="navigation">
-            <ul id="nav">
-            </ul>
-        </div>
-    </div>
-    <div id="main">
-        <div class="slide" id="first">
-            <div class="content">		
-                <h1>THIS IS SOME TEXT</h1>
-            </div>
-        </div>
-        <div class="slide" id="second">
-            <div class="content">		
-                <h1>THIS IS SOME MORE TEXT!</h1>
-                </div>
-        </div>
-        <div class="slide" id="third">
-            <div class="content">		
-                <h1>WHY YOU LOOKING AT THIS TEXT?</h1>
-                </div>
-        </div>
-    </div>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
-    </body>
-    </html>
-    );
-});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+  
 app.listen(PORT, HOST);
 console.log(`Running on
 http://${HOST}:${PORT}`);
