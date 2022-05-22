69d25301a0313225f88a9a69824911d8bd9d1c35
Adrian Brügger <63962845+adrianbruegger@users.noreply.github.com>
2022-05-22 18:34:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 // App
 const app = express();
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
+app.use(express.static(__dirname));
 app.listen(PORT, HOST);
 console.log(`Running on
 http://${HOST}:${PORT}`);
